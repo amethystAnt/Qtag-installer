bb7a0c5f8750994a21d92cc7b0be617824111a56
Karel Patlejch <kaja.patlejch@gmail.com>
2016-03-14 21:34:18 +0100

    Improved new_release.sh to edit the release date

--- new_release.sh
+++ new_release.sh
@@ -5,5 +5,11 @@ if [ -z $1 ]; then
 	exit
 fi
 
+if [ -z $2 ]; then
+	echo "Please specify the date of release in the yyyy-mm-dd format"
+	exit
+fi
+
 sed -i s/\<Version\>[^\"]*\</\<Version\>$1\</ src/config/config.xml
 sed -i s/\<Version\>[^\"]*\</\<Version\>$1\</ src/packages/com.qtag.qtag/meta/package.xml
+sed -i s/\<ReleaseDate\>[^\"]*\</\<ReleaseDate\>$2\</ src/packages/com.qtag.qtag/meta/package.xml
